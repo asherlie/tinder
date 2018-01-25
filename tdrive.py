5f2df6af6bab68192957feb4473aa5f855136b6c
Asher Lieber <asherlie@buffalo.edu>
2018-01-25 01:24:21 -0500

    added a lookup table for file block offset, made loading files towards bottom of file stack much faster

--- tdrive.py
+++ tdrive.py
@@ -18,6 +18,7 @@ class TinderStorage(object):
         self.gpg = gnupg.GPG()
         self.gpg.encoding = 'utf-8'
         self.key_id = -1
+        self.offset_table = {}
         keys = self.gpg.list_keys()
         if keys == []:
             print('please create a pgp key')
@@ -44,7 +45,7 @@ class TinderStorage(object):
     
     def store_file(self, filename, s_filename=None):
         p_f = self.prep_file_for_storage(filename, 900)
-        print('file will be stored in ' + str(len(p_f)) + ' messages')
+        print('file will be stored in ' + str(len(p_f)) + ' blocks')
         for i in p_f:
             self.t.send_message(self.mid, i)
         if s_filename != None: filename = s_filename
@@ -63,7 +64,24 @@ class TinderStorage(object):
         except ValueError:
             return -1
 
-    def load_file(self, filenum, out_fname=None):
+    def load_file(self, filenum, out_fname=None, use_ot=True):
+        # TODO: get rid of original implementation - load_file should only be called when there is an offset table
+        def chunk_to_data_str(c_b, c_e, o_fn):
+            data_str = ''
+            print('loading file \'' + o_fn + '\' from block ' + str(c_b) + ' to ' + str(c_e))
+            for i in range(c_b, c_e):
+                clean_msg = self.convo[i]['message']
+                if i == c_b:
+                    clean_msg = clean_msg[2::]
+                if self.convo[i]['message'][len(self.convo[i]['message'])-1] == "'":
+                    clean_msg = clean_msg[:-1:]
+                data_str += clean_msg
+            return data_str
+
+        if use_ot and self.offset_table != {}:
+            unb64 = base64.decodestring(bytes(chunk_to_data_str(self.offset_table[filenum][1], self.offset_table[filenum][2], self.offset_table[filenum][0]), 'utf-8').decode('unicode-escape').encode('utf-8'))
+            self.decrypt(unb64, '.tmp_encrypted_file.gpg', out_fname, self.pgp_pp)
+            return
         data_str = ''
         c = 0
         msg = len(self.convo)-1
@@ -78,15 +96,7 @@ class TinderStorage(object):
                         if filenum == c: # if we've gotten to the correct file
                             if out_fname == None: out_fname = o_fname
                             range_st = msg-n_blocks  # -1 ?
-                            print('loading file \'' + o_fname + '\' from block ' + str(range_st) + ' to ' + str(msg))
-                            for i in range(range_st, msg):
-                                # TODO: handle removal of unneeded b' in store_file
-                                clean_msg = self.convo[i]['message']
-                                if i == range_st:
-                                    clean_msg = clean_msg[2::]
-                                if self.convo[i]['message'][len(self.convo[i]['message'])-1] == "'":
-                                    clean_msg = clean_msg[:-1:]
-                                data_str += clean_msg
+                            data_str = chunk_to_data_str(range_st, msg, o_fname)
                             break
                         # filenum != c but we found a valid descriptor block
                         else:
@@ -97,7 +107,12 @@ class TinderStorage(object):
         unb64 = base64.decodestring(bytes(data_str, 'utf-8').decode('unicode-escape').encode('utf-8'))
         self.decrypt(unb64, '.tmp_encrypted_file.gpg', out_fname, self.pgp_pp)
 
-    def list_files(self):
+    def list_files(self, silent=False, use_ot=True):
+        if use_ot and self.offset_table != {}:
+            for i in self.offset_table:
+                print('file ' + str(i) + ': ' + self.offset_table[i][0])
+                print('occupies ' + str(self.offset_table[i][2]-self.offset_table[i][1]) + ' blocks')
+            return
         c = 0
         msg = len(self.convo)-1
         while msg >= 0:
@@ -109,8 +124,10 @@ class TinderStorage(object):
                     if tmp_decrypted.data.decode().split(' ')[0].isnumeric():
                         n_blocks = int(tmp_decrypted.data.decode().split(' ')[0])
                         o_fname = tmp_decrypted.data.decode()[len(str(n_blocks))+1::]
-                        print('file ' + str(c) + ': ' + o_fname) 
-                        print('occupies ' + str(n_blocks) + ' blocks')
+                        self.offset_table[c] = (o_fname, msg-n_blocks, msg)
+                        if not silent:
+                            print('file ' + str(c) + ': ' + o_fname) 
+                            print('occupies ' + str(n_blocks) + ' blocks')
                         c+=1
                         # ignores the file blocks, we only care about descriptor blocks
                         msg -= n_blocks
@@ -121,6 +138,7 @@ class TinderStorage(object):
         for i in m:
             if i['id'] == self.mid:
                 self.convo = i['messages']
+        self.list_files(True, False)
         
     # TODO: move this into store_file for speed
     def sp(self, st, le):
